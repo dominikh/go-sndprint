4c253d5447249bdd0861ae4e09cfd061f64f1443
Dominik Honnef <dominik@honnef.co>
2018-11-20 23:51:52 +0100

    Basic indexing and search

--- sndprint.go
+++ sndprint.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
 	"fmt"
 	"io"
 	"log"
@@ -10,8 +11,22 @@ import (
 	"math/cmplx"
 	"os"
 
+	"github.com/lib/pq"
 	"github.com/mjibson/go-dsp/fft"
+	"honnef.co/go/spew"
 )
+
+/*
+CREATE TABLE hashes (
+  id SERIAL PRIMARY KEY,
+  hash INTEGER NOT NULL,
+  song UUID NOT NULL,
+  off INTEGER NOT NULL
+);
+
+CREATE INDEX ON hashes (hash);
+CREATE INDEX ON hashes (song, off);
+*/
 
 // These bins correspond to 33 bins in the range 300–3000 Hz with
 // logarithmic spacing between them, akin to a Bark scale. These bins
@@ -130,7 +145,127 @@ func hashes(path string) []uint32 {
 	return hashes
 }
 
+func ber(s1, s2 []uint32) float64 {
+	e := 0
+	for i := range s1 {
+		e += bits.OnesCount32(s1[i] ^ s2[i])
+	}
+	return float64(e) / (256 * 32)
+}
+
+const berCutoff = 0.25
+
 func main() {
+	db, err := sql.Open("postgres", "")
+	if err != nil {
+		panic(err)
+	}
+
+	type checkKey struct {
+		song     string
+		offStart int64
+		offEnd   int64
+	}
+	checked := map[checkKey]float64{}
+	h := hashes("/tmp/out3.raw")
+	h = h[:256]
+	for i, hash := range h {
+		rows, err := db.Query(`SELECT song, array_agg(off) FROM hashes WHERE hash = $1 GROUP BY song`, int32(hash))
+		if err != nil {
+			panic(err)
+		}
+		for rows.Next() {
+			var song []byte
+			var offs []int64
+			if err := rows.Scan(&song, pq.Array(&offs)); err != nil {
+				panic(err)
+			}
+
+			// OPT(dh): we can thin out the result set by coalescing sequential matches for the same song
+			for _, off := range offs {
+				if int64(i) > off {
+					// Impossible match, sample_n can't ever occur before song_n
+					continue
+				}
+
+				{
+					start, end := off-int64(i), off+256-int64(i)
+					if _, ok := checked[checkKey{string(song), start, end}]; ok {
+						// We've already checked this segment
+						continue
+					}
+					rows, err := db.Query(`SELECT hash FROM hashes WHERE song = $1 AND off >= $2 AND off <= $3 LIMIT 256`, song, off-int64(i), off+256-int64(i))
+					if err != nil {
+						panic(err)
+					}
+					hashes := make([]uint32, 0, 256)
+					for rows.Next() {
+						var hash int32
+						if err := rows.Scan(&hash); err != nil {
+							panic(err)
+						}
+						hashes = append(hashes, uint32(hash))
+					}
+					if rows.Err() != nil {
+						panic(rows.Err())
+					}
+					if len(hashes) != 256 {
+						// Too short
+						continue
+					}
+
+					checked[checkKey{string(song), start, end}] = ber(h, hashes)
+				}
+			}
+		}
+		if rows.Err() != nil {
+			panic(rows.Err())
+		}
+	}
+	spew.Dump(checked)
+}
+
+func main__() {
+	db, err := sql.Open("postgres", "")
+	if err != nil {
+		panic(err)
+	}
+
+	names := []string{
+		"/tmp/raw/2.raw",
+		"/tmp/raw/3.raw",
+		"/tmp/raw/4.raw",
+		"/tmp/raw/5.raw",
+		"/tmp/raw/6.raw",
+		"/tmp/raw/7.raw",
+		"/tmp/raw/8.raw",
+		"/tmp/raw/9.raw",
+		"/tmp/raw/10.raw",
+	}
+	uuids := []string{
+		"89e85d07-02ee-4902-a50f-6befda16954d",
+		"9e60c2e5-b717-46dd-a0b9-4d55f1802a45",
+		"fe24611f-af64-4966-b135-92e75a06dbff",
+		"b9622a3a-a1f0-4326-adf3-9ab41df881af",
+		"198dfc8d-045e-498b-9abd-a5bb6ff0ef02",
+		"72a57154-72bc-49e9-9e61-22f60a6339da",
+		"264d5697-f505-492e-a539-81067115d6f3",
+		"d69433b6-5130-4f60-bdd9-399d973d827d",
+		"eefba0f9-1df4-4261-bf26-e78f3184462f",
+	}
+	for i, name := range names {
+		fmt.Println(name)
+		h1 := hashes(name)
+		for off, h := range h1 {
+			_, err := db.Exec(`INSERT INTO hashes (hash, song, off) VALUES ($1, $2, $3)`, int32(h), uuids[i], off)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+func main_() {
 	h1 := hashes(os.Args[1])
 	h2 := hashes(os.Args[2])
 
