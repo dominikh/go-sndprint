3aa7ebdbdb716953d7b3ff86d38842dad6a9b388
Dominik Honnef <dominik@honnef.co>
2018-11-24 21:23:43 +0100

    Reduce number of queries by merging overlapping candidates

--- cmd/sndprint-query/main.go
+++ cmd/sndprint-query/main.go
@@ -130,15 +130,18 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
-			if len(hh[0]) != len(h[0]) {
-				continue
-			}
 
-			var res [len(h)]float64
-			for k := range hh {
-				res[k] = ber(h[k], hh[k])
+			for off := 0; off <= c.span; off++ {
+				if len(hh[0][off:]) < len(h[0]) {
+					continue
+				}
+
+				var res [len(h)]float64
+				for k := range hh {
+					res[k] = ber(h[k], hh[k][off:len(hh[k])-(c.span-off)])
+				}
+				bers = append(bers, result{c.song, c.rng, res})
 			}
-			bers = append(bers, result{c.song, c.rng, res})
 		}
 		sort.Slice(bers, func(i, j int) bool {
 			var s1, s2 float64
@@ -206,6 +209,8 @@ func fetchHashes(db *pgx.Conn, song string, start, end int) ([4][]uint32, error)
 type candidate struct {
 	song string
 	rng  [2]int
+
+	span int
 }
 
 func fetchCandidates(db *pgx.Conn, h [4][]uint32) ([]candidate, error) {
@@ -239,10 +244,17 @@ WHERE (hash0 = ANY ($1) OR hash1 = ANY ($2) OR hash2 = ANY ($3) OR hash3 = ANY (
 			for i, hh := range h[k] {
 				if int32(hh) == hashes[k] {
 					start, end := off-i, off+len(h[0])-i-1
-					candidateScores[candidate{song, [2]int{start, end}}]++
+					if start < 0 {
+						continue
+					}
+					candidateScores[candidate{song: song, rng: [2]int{start, end}}]++
 				}
 			}
 		}
+	}
+
+	if len(candidateScores) == 0 {
+		return nil, nil
 	}
 
 	var candidates []candidate
@@ -250,5 +262,31 @@ WHERE (hash0 = ANY ($1) OR hash1 = ANY ($2) OR hash2 = ANY ($3) OR hash3 = ANY (
 		candidates = append(candidates, k)
 	}
 
-	return candidates, nil
+	sort.Slice(candidates, func(i, j int) bool {
+		if candidates[i].song != candidates[j].song {
+			return candidates[i].song < candidates[j].song
+		}
+		if candidates[i].rng[0] != candidates[j].rng[0] {
+			return candidates[i].rng[0] < candidates[j].rng[0]
+		}
+		return candidates[i].rng[1] < candidates[j].rng[1]
+	})
+
+	merged := make([]candidate, 0, len(candidates))
+	merged = append(merged, candidates[0])
+	for _, x := range candidates[1:] {
+		m := &merged[len(merged)-1]
+
+		if x.song == m.song &&
+			x.rng[0] == m.rng[0]+1+m.span &&
+			x.rng[1] == m.rng[1]+1 {
+
+			m.rng[1]++
+			m.span++
+		} else {
+			merged = append(merged, x)
+		}
+	}
+
+	return merged, nil
 }
